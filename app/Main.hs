3627264adb894783111f31a2606d4001aa6e6ffd
Joseph Walker <joseph.walker@outlook.com>
2022-10-23 19:39:08 -0500

    Hookup CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,40 +1,114 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+
 module Main where
 
 import System.Process
-import System.Exit
-import System.IO
 import Sources.Pnpm ( parsePnpmAudit )
 import Data.Dependencies ( Package )
 import Lib.Report ( createReports, printReports )
 import Control.Monad.Except
 import Control.Exception
+import Options.Applicative
+
 import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy.Char8 as BSC
+
+import System.Exit (ExitCode(ExitSuccess))
 
 type Task = ExceptT String IO
 
-runPnpmList :: Task BS.ByteString
-runPnpmList = do
-    (_, stdOut, stdErr, process) <- liftIO $ createProcess (proc "pnpm" ["list", "-r", "--json"])
-    case BS.hGetContents <$> stdOut of
-        Just h ->
-            liftIO h
-        Nothing ->
-            throwError "Error executing pnpm command"
+data InputSource
+    = FileInput FilePath
+    | StdInput
+    | InputNone
 
-runPnpmReadAuditFile :: Task BS.ByteString
-runPnpmReadAuditFile = do
-    fileOrErr <- liftIO (try $ BS.readFile "./deps.json" :: IO (Either IOException BS.ByteString))
-    either (\_ -> throwError "Error reading file") return fileOrErr
+data AuditInstalledOptions = AuditInstalledOptions
+    { auditInstalledInput :: InputSource
+    }
 
-createReport :: ExceptT String IO [Package]
-createReport = do
-    pnpmListOutput <- runPnpmReadAuditFile
-    either throwError return (parsePnpmAudit pnpmListOutput)
+data Command
+    = AuditInstalled AuditInstalledOptions
+
+data MonoTools = MonoTools
+    { optCommand :: Command
+    }
+
+fileInput :: Parser InputSource
+fileInput = FileInput <$> strOption
+    (  long "file"
+    <> short 'f'
+    <> metavar "TARGET"
+    <> help "Path to dependencies.json file from pnpm list"
+    )
+
+stdInput :: Parser InputSource
+stdInput = flag' StdInput
+  (  long "stdin"
+  <> help "Read pnpm list from stdin"
+  )
+
+inputSource :: Parser InputSource
+inputSource =
+    fileInput <|> stdInput <|> pure InputNone
+
+auditInstalledCommand :: Parser Command
+auditInstalledCommand =
+    AuditInstalled . AuditInstalledOptions <$> inputSource
+
+monoTools :: Parser MonoTools
+monoTools = MonoTools <$>
+    hsubparser
+        ( command "audit-installed" (info auditInstalledCommand ( progDesc "String")) )
 
 main :: IO ()
-main = do
-    audit <- runExceptT createReport
-    let reports = createReports <$> audit
-    case reports of
-        Left err -> print err
-        Right reports' -> printReports reports'
+main =
+    runMonoTools =<< execParser opts
+    where
+        opts = info (monoTools <**> helper)
+            (  fullDesc
+            <> progDesc "Tooling for managing dependencies in a PNPM / Turbo monorepo"
+            )
+
+runMonoTools :: MonoTools -> IO ()
+runMonoTools monoToolsOptions =
+    case optCommand monoToolsOptions of
+        AuditInstalled auditInstalledOptions ->
+            runAuditInstalledCommand auditInstalledOptions
+
+runAuditInstalledCommand :: AuditInstalledOptions -> IO ()
+runAuditInstalledCommand opts = do
+    result <- runExceptT runAuditInstalledCommand'
+    case result of
+        Left err -> putStrLn $ "Err: " ++ err
+        Right _  -> pure ()
+    where
+        runAuditInstalledCommand' = do
+            input <- case auditInstalledInput opts of
+                FileInput file ->
+                    getInputFromFile file
+                _ ->
+                    getInputFromPnpmList
+            audit <- either throwError return (parsePnpmAudit input)
+            let reports = createReports audit
+            liftIO $ printReports reports
+
+getInputFromPnpmList :: Task BS.ByteString
+getInputFromPnpmList = do
+    (code, stdOut, stdErr) <- liftIO $ readProcessWithExitCode "pnpm" ["list", "-r", "--json"] ""
+    case code of
+        ExitSuccess ->
+            -- For some reason pnpm list doesn't exit with a bad status code if the directory has no package.jsons
+            if stdOut == "" then
+                throwError genericError
+            else
+                return $ BSC.pack stdOut
+        _ ->
+            throwError stdErr
+    where
+        genericError = "Error executing pnpm command - does this directory contain a package.json?"
+
+getInputFromFile :: FilePath -> Task BS.ByteString
+getInputFromFile inputFile = do
+    fileOrErr <- liftIO (try $ BS.readFile inputFile :: IO (Either IOException BS.ByteString))
+    either (\_ -> throwError ("Unable to open file - does " ++ inputFile ++ " exist?")) return fileOrErr
